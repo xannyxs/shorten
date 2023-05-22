7db5917268ffa9a17efa315d0ff1020d79d9942b
Xander <xandervoorvaart@protonmail.com>
2023-05-23 00:40:50 +0200

    Now properly puts text in next cell

--- src/python/saveToDatabase.py
+++ src/python/saveToDatabase.py
@@ -1,19 +1,14 @@
 import gspread
 import authGoogleAccount
 from gspread import Worksheet
-from typing import Union
 
 
 def save_to_database(video_file: str, summary: str) -> None:
     sheet: Worksheet = authGoogleAccount.open_sheet("19pYdNcxu1zWogl_TZukCFEv5HguKBkD_0IT1aRldRPs")
 
-    # Retrieve all records of the data
-    records_data = sheet.get_all_records()
+    values_list = sheet.col_values(1)
+    empty_row = len(values_list) + 1
 
-    # Find the first empty row
-    empty_row = len(records_data) + 1
-
-    # Write to the first empty row
     sheet.update('A' + str(empty_row), video_file)
     sheet.update('B' + str(empty_row), summary)
 

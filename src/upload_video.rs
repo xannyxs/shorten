941ecb80f3e04290d09e558f8c757f69656a5ec3
Xander <xandervoorvaart@protonmail.com>
2023-06-14 00:12:48 +0200

    Now shows Python stderr

--- src/upload_video.rs
+++ src/upload_video.rs
@@ -1,9 +1,10 @@
 use std::io;
 use std::path::Path;
 use std::process::Command;
+use std::str;
 
 pub fn call_python_script(video_path: &Path, asset_id: &str) -> io::Result<()> {
-    let script_path = "/shorten/src/python/transcodeVideo.py";
+    let script_path = "./src/python/transcodeVideo.py";
 
     let output = Command::new("python3")
         .arg(script_path)
@@ -12,9 +13,17 @@ pub fn call_python_script(video_path: &Path, asset_id: &str) -> io::Result<()> {
         .output()?;
 
     if !output.status.success() {
+        let stderr =
+            str::from_utf8(&output.stderr).unwrap_or("Failed to parse python script stderr output");
+        let stdout =
+            str::from_utf8(&output.stdout).unwrap_or("Failed to parse python script stdout output");
+
         return Err(io::Error::new(
             io::ErrorKind::Other,
-            "Python script ended with error",
+            format!(
+                "Python script ended with error:\nStderr:\n{}\nStdout:\n{}",
+                stderr, stdout
+            ),
         ));
     }
 

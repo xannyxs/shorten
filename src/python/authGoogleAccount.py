7229d2ef7e8e7b7e101f0b94861abd0dfbb952b3
xvoorvaa <xander@xvproductions.com>
2023-07-14 13:24:42 +0200

    Added GCP_Credentials

--- src/python/authGoogleAccount.py
+++ src/python/authGoogleAccount.py
@@ -1,18 +1,18 @@
 import gspread
+import os
 from google.oauth2.service_account import Credentials
 from googleapiclient import discovery
 from gspread import Client, Spreadsheet, Worksheet
 from typing import Any, Optional
 
-
-def login_drive() -> Optional[Any]:
-    creds = Credentials.from_service_account_file("credentials.json")
-    return discovery.build('drive', 'v3', credentials=creds)
+# def login_drive() -> Optional[Any]:
+# creds = Credentials.from_service_account_file("credentials.json")
+# return discovery.build('drive', 'v3', credentials=creds)
 
 
 # Hardcoded path to credentials.json
 def login_gspread() -> Client:
-    return gspread.service_account("./credentials.json")
+    return gspread.service_account_from_dict(os.getenv('GCP_CREDENTIALS'))
 
 
 def open_sheet(spread_id: str) -> Worksheet:

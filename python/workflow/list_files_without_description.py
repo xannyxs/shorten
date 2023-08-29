8837a1ef1c8b852fd58ba084af39a97f21c99bea
xvoorvaa <xander@xvproductions.com>
2023-08-29 15:43:03 +0200

    Changed the workflow to make automatic desciptions

--- python/workflow/list_files_without_description.py
+++ python/workflow/list_files_without_description.py
@@ -1,19 +1,22 @@
 import json
-from typing import List
-
 import glob
+import os
+from typing import List
 
 
 def list_files_without_description(batch_size: int = 10) -> List[List[str]]:
     files_without_description: List[str] = []
 
-    for file_path in glob.glob('./streameth-platform/data/session/**/*.JSON', recursive=True):
+    file_paths = glob.glob(
+        './streameth-platform/data/sessions/ethberlin_2022/*.json', recursive=True)
+
+    for file_path in file_paths:
         with open(file_path, 'r') as f:
             data = json.load(f)
-            if not data.get('gpt_description') or data['gpt_description'].strip() == '':
+            if not data.get('gpt_description') or data['gpt_description'].strip() == '' or not data.get('description') or data['description'].strip() == '':
                 files_without_description.append(file_path)
 
-    batches: List[List[str]] = [files_without_description[i:i+batch_size]
+    batches: List[List[str]] = [files_without_description[i:i + batch_size]
                                 for i in range(0, len(files_without_description), batch_size)]
 
     return batches
@@ -21,5 +24,4 @@ def list_files_without_description(batch_size: int = 10) -> List[List[str]]:
 
 if __name__ == "__main__":
     batches = list_files_without_description()
-
     print(json.dumps(batches))
